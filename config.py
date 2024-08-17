560128ab037725c2518586789e78a04f4c6a102a
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-08-17 19:33:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -50,7 +50,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ChampuXD/ChampuMusic",
+    "https://github.com/Rishabhopi/VipChampuMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
